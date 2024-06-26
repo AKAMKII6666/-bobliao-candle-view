de8517dadfd3fbdb4019258e632ac8d88b6adc83
AKA MKII <58581967+AKAMKII6666@users.noreply.github.com>
2024-06-26 22:44:24 +0800

    Improved rendering speed
    Improved rendering speed

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { candleViewContext, useCandleView, useCandleViewContext, } from './content/context/candleViewContext';
-export * from './content/interface/basicShapesInterFace';
-export * from './content/interface/configInterFaces';
-export * from './content/interface/contextInterFace';
-export * from './content/interface/hooksInterFace';
-export * from './content/interface/itemsInterFace';
-export * from './content/interface/timeDefineInterFace';
-export { CandleView } from './content/index';
