3f0c41ac0055e9a5e1fb6ca468f3448f689312b2
AKA MKII <58581967+AKAMKII6666@users.noreply.github.com>
2026-03-14 05:03:32 +0800

    fix: 清理package.json依赖，移至devDependencies

--- dist/content/hooks/throttleHook.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * 节流钩子
- */
-declare const useThrottle: () => (_func: () => void, _time: number) => void;
-export default useThrottle;
--- dist/content/hooks/yAxisHook.d.ts
+++
@@ -1,7 +0,0 @@
-import { Iaxis } from "../interface/configInterFaces";
-import { IAxisobj, IyAxisobj } from "../interface/hooksInterFace";
-/**
- * y轴钩子
- */
-declare const useyAxis: (args: Iaxis, xAxis?: IAxisobj) => IyAxisobj;
-export default useyAxis;
